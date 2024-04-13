bd101ce43bee702084a789da8d50876320563768
ya.gasparyan <ya.gasparyan@gaskar.group>
2024-04-13 16:02:51 +0300

    add mui, styled-component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { WithYmaps } from './YsMap'
 import { YMaps } from '@pbe/react-yandex-maps'
-
+import { Box } from '@mui/material'
 function App() {
   const data = [
     { pointName: 'One', pointX: 37.573, pointY: 55.751 },
@@ -8,11 +8,11 @@ function App() {
   ]
   return (
     <>
-      <div>
+      <Box>
         <YMaps query={{ apikey: '8bb5cdf6-83a2-449c-ab22-ab2b1e60ca13' }}>
           <WithYmaps data={data} />
         </YMaps>
-      </div>
+      </Box>
     </>
   )
 }
